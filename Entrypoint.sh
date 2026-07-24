d580be6489ec5fe238e7a1d92d0892b393e1d57c
i8o8i Developer <228196009+i8o8i-Developer@users.noreply.github.com>
2026-07-24 21:28:01 +0530

    Update Entrypoint.sh

--- Entrypoint.sh
+++ Entrypoint.sh
@@ -1,9 +1,8 @@
 #!/bin/bash
 set -e
-
 INSTALL_DIR=/home/steam/nmrih
 
-echo "=== Installing / updating No More Room in Hell dedicated server ==="
+echo "=== Installing / Udating No More Room In Hell Dedicated Server ==="
 steamcmd +force_install_dir "$INSTALL_DIR" \
   +login anonymous \
   +app_update 317670 validate \
@@ -11,17 +10,16 @@ steamcmd +force_install_dir "$INSTALL_DIR" \
 
 cd "$INSTALL_DIR"
 
-
 HOSTNAME_TITLE=$(echo "${SRCDS_HOSTNAME:-NMRIH Durgaai Solutions}" | \
   awk '{for(i=1;i<=NF;i++){$i=toupper(substr($i,1,1)) substr($i,2)}}1')
 
-echo "=== Starting Server: $HOSTNAME_TITLE ==="
+echo "=== Starting Server : $HOSTNAME_TITLE ==="
 
 exec ./srcds_run \
   -game nmrih \
   -console \
   -usercon \
-  +port ${SRCDS_PORT:-27015} \
+  -port ${SRCDS_PORT:-27015} \
   +map ${SRCDS_MAP:-nmo_broadway} \
   +maxplayers ${SRCDS_MAXPLAYERS:-8} \
   +hostname "$HOSTNAME_TITLE" \
